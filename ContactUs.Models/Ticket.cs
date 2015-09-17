19d37b3cacf10f5c904bcca48e18a7058a3b7feb
Tanthawat Khemavast <nonamexp9@hotmail.com>
2015-09-17 09:28:37 +0700

    Add sub projects

---
+++ ContactUs.Models/Ticket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactUs.Models
+{
+    public class Ticket
+    {
+
+        public string Id { get; set; }
+        public int Title { get; set; }
+        public string Body { get; set; }
+        public TicketStatus Status { get; set; }
+
+        public void Accept()
+        {
+
+        }
+
+        public void Close()
+        {
+
+        }
+
+        public void Reject()
+        {
+
+        }
+    }
+}
